cd94d8728622c7afd420ddd5fc456debe4dd6bb7
minji73 <kimminji7373@gmail.com>
2025-02-21 16:26:24 +0900

    ㅊ

--- 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/FooterArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/03.구현소스/dc-app/src/components/layout/FooterArea.jsx
@@ -1,12 +1,39 @@
 /// 하단영역 컴포넌트 : FooterArea.jsx /////
 
-export default function FooterArea(){
+// 하단메뉴 데이터 불러오기 ////
+import { bmData } from "../../js/data/bmenu";
 
-    /// 리턴 코드구역 ////////
-    return (
-        <>
-            <h1>하단영역 컴포넌트</h1>
-        </>
-    );
+// 하단영역 CSS 불러오기 ////
+import '../../css/common/footer_area.scss';
 
+export default function FooterArea() {
+  /// 리턴 코드구역 ////////
+  return (
+    <footer className="info">
+      <ul>
+        {/* 하단로고 컴포넌트 넣기 */}
+        <li>
+          하단로고
+        </li>
+        <li>
+          {/* 하단링크박스 */}
+          <ol className="bmenu">
+            {
+                bmData.map((v,i)=>
+                <li key={i}>
+                    <a 
+                        href={v.link} 
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        {v.txt.toUpperCase()}
+                    </a>
+                </li>)
+            }
+          </ol>
+        </li>
+        <li>© & ™ DC. ALL RIGHTS RESERVED</li>
+      </ul>
+    </footer>
+  );
 } //////////// FooterArea 컴포넌트 ///////////
